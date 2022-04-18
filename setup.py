4329c9404caa6145d28a30bf76d2a975457dddce
Antoine Beyeler <abeyeler@ab-ware.com>
2022-04-18 14:40:45 +0200

    Bumped version to 0.2.0a1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("LICENSE") as f:
 
 setup(
     name="vpype-pixelart",
-    version="0.1.0",
+    version="0.2.0a1",
     description="Pixel art plug-in for vpype",
     long_description=readme,
     long_description_content_type="text/markdown",
