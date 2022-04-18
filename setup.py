4e47df2a926f5779749810b2e3bebc075cd0bdf9
Antoine Beyeler <abeyeler@ab-ware.com>
2022-04-18 14:26:27 +0200

    Finishing touches

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("LICENSE") as f:
 
 setup(
     name="vpype-pixelart",
-    version="0.1.0a5",
+    version="0.1.0a6",
     description="Pixel art plug-in for vpype",
     long_description=readme,
     long_description_content_type="text/markdown",
