1154318bcd3e78df0fc12432545650af777eb24a
Antoine Beyeler <49431240+abey79@users.noreply.github.com>
2022-04-18 14:35:02 +0200

    Add basic release setup (#1)
    - add release workflow
    - cleaned-up setup.py

--- setup.py
+++ setup.py
@@ -8,14 +8,20 @@ with open("LICENSE") as f:
 
 setup(
     name="vpype-pixelart",
-    version="0.1.0",
-    description="",
+    version="0.1.0a6",
+    description="Pixel art plug-in for vpype",
     long_description=readme,
     long_description_content_type="text/markdown",
     author="Antoine Beyeler",
-    url="",
-    license=license,
+    url="https://github.com/abey79/vpype-pixelart",
     packages=["pixelart"],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Multimedia :: Graphics",
+        "Environment :: Plugins",
+    ],
+    setup_requires=["wheel"],
     install_requires=[
         "click",
         "imageio",
